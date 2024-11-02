15effc1fbc5c1d637235fc6e8a6b751673a3e374
yuma <uenyum18@gmail.com>
2024-11-02 17:22:09 +0400

    import削除

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import datetime
-import matplotlib.pyplot as plt
 import json
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
